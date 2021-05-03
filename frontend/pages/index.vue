7c18d1b1004a55f625abe2c49b67ad5a09fec089
Marantesss <up201706917@fe.up.pt>
2021-05-03 11:00:45 +0100

    :poop: :beers: :package: add vue vis wrapper

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,141 +1,49 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">frontend</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-    <div id="arc"></div>
-  </div>
+  <Network
+    id="main-network"
+    ref="mynetwork"
+    :nodes="nodes"
+    :edges="edges"
+    :options="options"
+  >
+  </Network>
 </template>
 
 <script>
-import * as d3 from 'd3'
-
 export default {
-  name: 'Index',
+  name: 'Vis',
 
   data() {
     return {
-      gdp: [
-        { country: 'USA', value: 20.5 },
-        { country: 'China', value: 13.4 },
-        { country: 'Germany', value: 4.0 },
-        { country: 'Japan', value: 4.9 },
-        { country: 'France', value: 2.8 },
+      // create an array with nodes
+      nodes: [
+        { id: 1, label: 'Node 1' },
+        { id: 2, label: 'Node 2' },
+        { id: 3, label: 'Node 3' },
+        { id: 4, label: 'Node 4' },
+        { id: 5, label: 'Node 5' },
       ],
+      // create an array with edges
+      edges: [
+        { from: 1, to: 3 },
+        { from: 1, to: 2 },
+        { from: 2, to: 4 },
+        { from: 2, to: 5 },
+      ],
+      options: {},
     }
-  },
-
-  mounted() {
-    this.generateArc()
-  },
-  methods: {
-    generateArc() {
-      const w = 500
-      const h = 500
-
-      const svg = d3
-        .select('#arc')
-        .append('svg')
-        .attr('width', w)
-        .attr('height', h)
-
-      const sortedGDP = this.gdp.sort((a, b) => (a.value > b.value ? 1 : -1))
-      const color = d3.scaleOrdinal(d3.schemeDark2)
-
-      const maxGDP = d3.max(sortedGDP, (o) => o.value)
-
-      const angleScale = d3
-        .scaleLinear()
-        .domain([0, maxGDP])
-        .range([0, 1.5 * Math.PI])
-
-      const arc = d3
-        .arc()
-        .innerRadius((d, i) => (i + 1) * 25)
-        .outerRadius((d, i) => (i + 2) * 25)
-        .startAngle(angleScale(0))
-        .endAngle((d) => angleScale(d.value))
-
-      const g = svg.append('g')
-
-      g.selectAll('path')
-        .data(sortedGDP)
-        .enter()
-        .append('path')
-        .attr('d', arc)
-        .attr('fill', (d, i) => color(i))
-        .attr('stroke', '#FFF')
-        .attr('stroke-width', '1px')
-        .on('mouseenter', function () {
-          d3.select(this).transition().duration(200).attr('opacity', 0.5)
-        })
-        .on('mouseout', function () {
-          d3.select(this).transition().duration(200).attr('opacity', 1)
-        })
-
-      g.selectAll('text')
-        .data(this.gdp)
-        .enter()
-        .append('text')
-        .text((d) => `${d.country} -  ${d.value} Trillion`)
-        .attr('x', -150)
-        .attr('dy', -8)
-        .attr('y', (d, i) => -(i + 1) * 25)
-
-      g.attr('transform', 'translate(200,300)')
-    },
   },
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
+#main-network {
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  bottom: 0px;
+  right: 0px;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  /* background-color: aquamarine; */
 }
 </style>
