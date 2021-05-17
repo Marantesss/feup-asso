e8179d26844242b6ab5e3979c4e4b02f76ce554b
Marantesss <up201706917@fe.up.pt>
2021-05-17 09:43:23 +0100

    :package: add cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,15 @@
+import cors from 'cors';
 import express from 'express';
 import getDependencies from './dependency-manager';
 
 const app = express();
 const port = 3000; // default port to listen
+
+// middlewares
+const options: cors.CorsOptions = {
+  origin: '*',
+};
+app.use(cors(options));
 
 app.get('/', async (req, res) => {
   const path = 'src/dependency-manager/src/projects/2';
